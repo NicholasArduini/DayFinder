7bbec1158e7658ca86d723d9b0778789b8296575
Nicholas Arduini <NicholasArduini@GLaDOS.local>
2015-05-07 23:28:53 -0400

    Initial Commit

---
+++ Day Finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day Finder
+//
+//  Created by Nicholas Arduini on 2015-05-07.
+//  Copyright (c) 2015 Nicholas Arduini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
